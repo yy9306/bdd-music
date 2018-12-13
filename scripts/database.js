4ae6e50eacc6fcdd45487a2e0f99200c22fca158
jungege <>
2018-12-14 00:30:09 +0800

    music

---
+++ scripts/database.js
@@ -0,0 +1,87 @@
+var database = [
+    {
+        "filename": "./musics/等你下课(with 杨瑞代).mp4",
+        "nb_streams": 1,
+        "nb_programs": 0,
+        "format_name": "mp4",
+        "format_long_name": "MP2/3 (MPEG audio layer 2/3)",
+        "start_time": "0.025057",
+        "duration": "270.071293",
+        "size": "3173444",
+        "bit_rate": "128028",
+        "probe_score": 51,
+        "tags": {
+            "title": "等你下课",
+            "album": "Featured Of World Classical Music CD6",
+            "artist": "周杰伦"
+        }
+    },
+    {
+        "filename": "./musics/晴天.mp4",
+        "nb_streams": 1,
+        "nb_programs": 0,
+        "format_name": "mp4",
+        "format_long_name": "MP2/3 (MPEG audio layer 2/3)",
+        "start_time": "0.025057",
+        "duration": "269.815873",
+        "size": "2573676",
+        "bit_rate": "128035",
+        "probe_score": 51,
+        "tags": {
+            "title": "晴天",
+            "album": "Featured Of World Classical Music CD6",
+            "artist": "周杰伦"
+        }
+    },
+    {
+        "filename": "./musics/不能说的秘密.mp4",
+        "nb_streams": 1,
+        "nb_programs": 0,
+        "format_name": "mp3",
+        "format_long_name": "MP2/3 (MPEG audio layer 2/3)",
+        "start_time": "0.025057",
+        "duration": "296.611701",
+        "size": "3245757",
+        "bit_rate": "128028",
+        "probe_score": 51,
+        "tags": {
+            "title": "不能说的秘密",
+            "album": "Featured Of World Classical Music CD6",
+            "artist": "周杰伦"
+        }
+    },
+    {
+        "filename": "./musics/青花瓷.mp4",
+        "nb_streams": 1,
+        "nb_programs": 0,
+        "format_name": "mp3",
+        "format_long_name": "MP2/3 (MPEG audio layer 2/3)",
+        "start_time": "0.025057",
+        "duration": "292.806531",
+        "size": "4685624",
+        "bit_rate": "128019",
+        "probe_score": 51,
+        "tags": {
+            "title": "青花瓷",
+            "album": "Featured Of World Classical Music CD6",
+            "artist": "周杰伦"
+        }
+    },
+    {
+        "filename": "./musics/烟花易冷.mp4",
+        "nb_streams": 1,
+        "nb_programs": 0,
+        "format_name": "mp3",
+        "format_long_name": "MP2/3 (MPEG audio layer 2/3)",
+        "start_time": "0.025057",
+        "duration": "236.800000",
+        "size": "3789514",
+        "bit_rate": "128024",
+        "probe_score": 51,
+        "tags": {
+            "title": "烟花易冷",
+            "album": "Featured Of World Classical Music CD6",
+            "artist": "周杰伦"
+        }
+    }
+]
